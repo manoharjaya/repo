d33bd4820771037ec55c459b2bc95c5ea0af76e4
manoharjaya <manoharjaya.be@gmail.com>
2018-04-02 19:06:47 +0530

    understanding 2-simpleLinearRegressionUsingGradientDescentUsingwinequality-white.py apr 7:06 pm

--- myDocs/pgm/python/ml/2-linearRegression-GradientDescent/1-simpleLinearRegressionUsingGradientDescent.py
+++ myDocs/pgm/python/ml/2-linearRegression-GradientDescent/1-simpleLinearRegressionUsingGradientDescent.py
@@ -1,19 +1,3 @@
-'''
-# Make a prediction with coefficients
-def predict(row, coefficients):
-	yhat = coefficients[0]  # 0.4
-	for i in range(len(row)-1):  # 2-1   =1  range (1) 
-		print "row",row[i]
-		yhat += coefficients[i + 1] * row[i]     #  0.4+0.8*1
-	return yhat
-
-dataset = [[1, 1], [2, 3], [4, 3], [3, 2], [5, 5]]
-coef = [0.4, 0.8]
-for row in dataset:
-	yhat = predict(row, coef)    # pass 1 row Ans so on  like [1,1]
-	print("Expected=%.3f, Predicted=%.3f" % (row[-1], yhat))
-
-'''
 
 
 # Make a prediction with coefficients
@@ -25,7 +9,7 @@ def predict(row, coefficients):
 
 # Estimate linear regression coefficients using stochastic gradient descent
 def coefficients_sgd(train, l_rate, n_epoch):
-	coef = [0.0 for i in range(len(train[0]))]  # coef=[0.0,0.0] 
+	coef = [0.0 for i in range(len(train[0]))]  # [1,1] 
 	print "coef",coef   #[0.0 0.0]
 	for epoch in range(n_epoch):  # 5
 		sum_error = 0
