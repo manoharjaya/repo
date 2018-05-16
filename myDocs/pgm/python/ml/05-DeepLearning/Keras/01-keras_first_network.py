6dcbc3f9168d094c1af22328f1c352103bed7908
manharjaya <manoharjaya.be@gmail.com>
2018-05-16 11:06:54 +0000

    add DL

--- myDocs/pgm/python/ml/05-DeepLearning/Keras/01-keras_first_network.py
+++ myDocs/pgm/python/ml/05-DeepLearning/Keras/01-keras_first_network.py
@@ -1,32 +1,24 @@
-# Create your first MLP in Keras
+# Sample Multilayer Perceptron Neural Network in Keras
 from keras.models import Sequential
 from keras.layers import Dense
 import numpy
-# fix random seed for reproducibility
-numpy.random.seed(7)
-# load pima indians dataset
+# load and prepare the dataset
 dataset = numpy.loadtxt("dataset/pima-indians-diabetes.csv", delimiter=",")
-# split into input (X) and output (Y) variables
 X = dataset[:,0:8]
 Y = dataset[:,8]
-# create model
+# 1. define the network
 model = Sequential()
 model.add(Dense(12, input_dim=8, activation='relu'))
-model.add(Dense(8, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
-# Compile model
+# 2. compile the network
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-# Fit the model
-model.fit(X, Y, epochs=150, batch_size=10, verbose=2)
-
-'''
-#evaluate the model
-scores = model.evaluate(X, Y)
-print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
-'''
-
-# calculate predictions
-predictions = model.predict(X)
-# round predictions
-rounded = [round(x[0]) for x in predictions]
-print(rounded)
+# 3. fit the network
+history = model.fit(X, Y, epochs=100, batch_size=10)
+# 4. evaluate the network
+loss, accuracy = model.evaluate(X, Y)
+print("\nLoss: %.2f, Accuracy: %.2f%%" % (loss, accuracy*100))
+# 5. make predictions
+probabilities = model.predict(X)
+predictions = [float(round(x)) for x in probabilities]
+accuracy = numpy.mean(predictions == Y)
+print("Prediction Accuracy: %.2f%%" % (accuracy*100))
